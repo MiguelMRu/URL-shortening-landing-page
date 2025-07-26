57a7cb69512a18805b1fba2d182288674790f945
Miguel Mateos <miguelmateosruiz@gmail.com>
2025-07-26 09:50:27 +0200

    añasdir componentes y un estilar root

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import './App.css'
 import { Navigation } from './components/Navigation'
-
+import { Head } from './components/Head'
+import { Links } from './components/Links'
 function App() {
 
   /*Your users should be able to:
 
-- View the optimal layout for the site depending on their device's screen size
-- Shorten any valid URL
-- See a list of their shortened links, even after refreshing the browser
-- Copy the shortened link to their clipboard in a single click
-- Receive an error message when the `form` is submitted if:
-  - The `input` field is empty
+  - Shorten any valid URL
+  - See a list of their shortened links, even after refreshing the browser
+  - Copy the shortened link to their clipboard in a single click
+  - Receive an error message when the `form` is submitted if: The `input` field is empty
+  - View the optimal layout for the site depending on their device's screen size
+
 */
 
   return (
-    <Navigation/>
+    <>          
+      <Navigation/>
+      <Head/>
+      <Links/>
+    </>
   )
 }
 
